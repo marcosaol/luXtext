fe9f008e4934e59068f947195da439fc2e80704d
Marckos <marcoshenrique.oliveira2023@gmail.com>
2024-03-10 13:47:33 -0300

    feat: criação da funcionalidade do botão sobre

--- __init__.py
+++ __init__.py
@@ -1,16 +1,50 @@
 from tkinter import *
 from tkinter import ttk
-from definicoes import Menu
+from tkinter.ttk import * 
+from definicoes import Menu_
 
-menu_inicial = Tk()
-menu_inicial.title("LuxText")
-menu_inicial.resizable(True, True)
-menu_inicial.state("zoomed")
+root = Tk()
+
+menu_ = Menu_(window = root)
+
+root.title("LuxText")
+root.resizable(True, True)
+root.state("zoomed")
 
 largura = 800
 altura = 500
-menu_inicial.geometry(f"{largura}x{altura}")
-menu_inicial.minsize(largura, altura)
+root.geometry(f"{largura}x{altura}")
+root.minsize(largura, altura)
 
-menu_inicial.iconbitmap("imgs/Lx.png")
-menu_inicial.mainloop()
+ 
+menubar = Menu(root) 
+
+
+arquivo = Menu(menubar, tearoff = 0) 
+menubar.add_cascade(label ='Arquivo', menu = arquivo) 
+arquivo.add_command(label ='Novo arquivo', command = None) 
+arquivo.add_command(label ='Abrir', command = None) 
+arquivo.add_command(label ='Salvar', command = None) 
+arquivo.add_separator() 
+arquivo.add_command(label ='Sair', command = root.destroy) 
+  
+ 
+editar = Menu(menubar, tearoff = 0) 
+menubar.add_cascade(label ='Editar', menu = editar) 
+editar.add_command(label ='Recorta', command = None) 
+editar.add_command(label ='Copiar', command = None) 
+editar.add_command(label ='Colar', command = None) 
+editar.add_command(label ='Selecionar tudo', command = None) 
+editar.add_separator() 
+editar.add_command(label ='Achar', command = None) 
+
+configuracao = Menu(menubar, tearoff = 0) 
+menubar.add_cascade(label ='Configuração', menu = configuracao) 
+configuracao.add_command(label ='Ajuda', command = None) 
+configuracao.add_command(label ='Sobre', command = menu_.Sobre) 
+configuracao.add_separator() 
+configuracao.add_command(label ='Preferências', command = None) 
+  
+root.config(menu = menubar) 
+
+root.mainloop()
--- definicoes.py
+++ definicoes.py
@@ -1,14 +1,18 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter.ttk import * 
 
-class Menu:
-    def __init__(self,nome_arquivo = "Não salvo", texto_sobre = ""):
-        self.nome_arquivo = nome_arquivo
-        self.texto_sobre = texto_sobre
 
-    def arquivo(self, nome_arquivo):
-        self.nome_arquivo = nome_arquivo
-
-    def Sobre(self, texto_sobre):
-        texto_sobre = "Bem vindo ao LuxText um editor de texto feito com pyhton pensado para você"
-        self.texto_sobre = texto_sobre
+class Menu_:
+    def __init__(self, window):
+        self.window = window
+        self
+    def Sobre(self):
+       sobre = Toplevel(self.window)
+       sobre.title("Sobre")
+       sobre.geometry("800x500")
+       sobre_texto1 = Label(sobre, text="Bem vindo ao LuxText, seu editor de texto pensado para você!")
+       sobre_texto2 = Label(sobre, text="Implementado utilizando o Tkinter e python, versão beta 1.0")
+       sobre_texto1.pack(padx=10, pady=10,anchor="center")
+       sobre_texto2.pack(padx=10, pady=10,anchor="center")
+       sobre.mainloop()
